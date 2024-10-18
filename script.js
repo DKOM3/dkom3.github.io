c73f19f8ed01abc680a91fb1c27ad845276f5ac7
DKOM3 <danielkurushchak@gmail.com>
2024-10-18 11:26:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-onst quotes = [
+const quotes = [
     "The only way to do great work is to love what you do. - Steve Jobs",
     "Life is what happens when you're busy making other plans. - John Lennon",
     "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
